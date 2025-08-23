493bf8df857631ca99305ab81234b8761aab1c74
BrunTux <bruntux@gmail.com>
2025-08-22 23:30:20 -0400

    >:)

--- README.md
+++ README.md
@@ -2,7 +2,6 @@ Es una pagina de flask asi que pues la tienes que correr tu :)
 
 como hacerlo?
 
-```
 ```sh
 git clone https://github.com/brun-t/momazos-brun-t.git memes
 cd memes
--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from flask import Flask, render_template
 from livereload import Server
 
+"""Esta es la clase Flask que es lo que genera el server"""
 app = Flask(__name__)
-app.debug = True  # Required for template changes to reload
+
+
+"""
+Es necesario para livereload que es basicamente para que si cambias
+el HTML, el CSS o el Javascript se haga automaticamente
+"""
+
+
+app.debug = True
+
+
+"""
+Esta es la ruta principal osea lo que aparece con solo el
+nombre has visto como que ponen /login o /watch en youtube?
+pues esas son rutas y
+"/" es la principal en este caso solo mande el HTML
+"""
 
 
 @app.route("/")
@@ -11,12 +28,18 @@ def hello_world():
 
 
 if __name__ == "__main__":
+    # este es para livereload no es necesario en flask pero es util
     server = Server(app.wsgi_app)
 
-    # 👇 Watch templates and static files for changes
+    # Esto es para que "observe" los archivos que en este caso
+    # son todos los HTML
+    # en templates y todos los CSS y JS en static
     server.watch('templates/*.html')
     server.watch('static/**/*.css')
     server.watch('static/**/*.js')
 
-    # 👇 Start livereload server on http://localhost:5500
+    # Esto inicia el servidor en el puerto 5500 el puerto es simplemente un
+    # identificador de la pagina web
+    # El debug=True es para que en cambios de los archivos de python
+    # se actualize automaticamente sin necesidad de hacerlo tu mismo
     server.serve(port=5500, debug=True)
